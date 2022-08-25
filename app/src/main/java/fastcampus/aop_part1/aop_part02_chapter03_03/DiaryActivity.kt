31ae3530aa23ab6e0ed9dd0fc3249ad2f0975402
shinminjae <mailsmj@hanmail.net>
2022-08-25 14:19:09 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/aop_part1/aop_part02_chapter03_03/DiaryActivity.kt
@@ -0,0 +1,14 @@
+package fastcampus.aop_part1.aop_part02_chapter03_03
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView()
+    }
+
+}
