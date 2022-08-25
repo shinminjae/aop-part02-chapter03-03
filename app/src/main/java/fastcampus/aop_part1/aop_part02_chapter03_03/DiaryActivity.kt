b7a031b798fab379424ac4c5256313acc05e451e
shinminjae <mailsmj@hanmail.net>
2022-08-25 16:19:16 +0900

    마무리

--- app/src/main/java/fastcampus/aop_part1/aop_part02_chapter03_03/DiaryActivity.kt
+++ app/src/main/java/fastcampus/aop_part1/aop_part02_chapter03_03/DiaryActivity.kt
@@ -1,14 +1,43 @@
 package fastcampus.aop_part1.aop_part02_chapter03_03
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
 
 class DiaryActivity : AppCompatActivity() {
 
+    private val handler = Handler(Looper.getMainLooper()) //MainThread핸들러 하나가 만들어진거다.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView()
+        setContentView(R.layout.activity_diary)
+
+        var diaryEditText = findViewById<EditText>(R.id.diaryEditText)
+        val detailPreferences = getSharedPreferences("diary",Context.MODE_PRIVATE)
+
+        diaryEditText.setText(detailPreferences.getString("detail",""))
+
+        val runnable = Runnable {
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit {
+                putString("detail",diaryEditText.text.toString())
+            }
+
+            Log.d("DiaryActivity", "SAVE!!!!!! ${diaryEditText.text.toString()}")
+        }
+
+        diaryEditText.addTextChangedListener {
+
+            Log.d("DiaryActivity", "TextChanged :: $it")
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable,500)
+        }
     }
 
 }
--- app/src/main/java/fastcampus/aop_part1/aop_part02_chapter03_03/MainActivity.kt
+++ app/src/main/java/fastcampus/aop_part1/aop_part02_chapter03_03/MainActivity.kt
@@ -1,6 +1,7 @@
 package fastcampus.aop_part1.aop_part02_chapter03_03
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -59,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
             if (passwordPreferences.getString("password","000").equals(passwordFromUser)){
 
+                startActivity(Intent(this,DiaryActivity::class.java))
                 //패스워드 성공
                 //startActivity()
 
